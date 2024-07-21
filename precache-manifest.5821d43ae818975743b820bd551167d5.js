8954865b7f2624e2decbdef406677e9630f01e84
satyaprakash-02 <satyaprakash682002@gmail.com>
2024-07-21 11:28:58 +0530

    Updates

---
+++ precache-manifest.5821d43ae818975743b820bd551167d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3e610805060acd203a06e5ef74130d",
+    "url": "/AnalyzeIt-React/index.html"
+  },
+  {
+    "revision": "fc25f02e94fd728795fe",
+    "url": "/AnalyzeIt-React/static/css/main.82ba52a1.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/AnalyzeIt-React/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/AnalyzeIt-React/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "fc25f02e94fd728795fe",
+    "url": "/AnalyzeIt-React/static/js/main.f75bdef6.chunk.js"
+  },
+  {
+    "revision": "c6fa2b7e6a6d4429780f",
+    "url": "/AnalyzeIt-React/static/js/runtime~main.a165c346.js"
+  }
+]);
